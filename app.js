130defe366e7dc4d8bb0acf870412f5bf6208cab
Maicle72 <miga.verdugo@gmail.com>
2021-07-23 18:11:48 +0100

    Commit to fix things
    Fix ao botão "Saiba mais!", a fechar o menu quando carregar.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const linhas3 = document.querySelector(".header .nav-bar .nav-list .linhas3");
 const tlm_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
 const header = document.querySelector(".header.container");
 
 linhas3.addEventListener("click",()=>{
@@ -14,4 +15,11 @@ document.addEventListener("scroll", ()=>{
     }else{
         header.style.backgroundColor = "transparent";
     }
+});
+
+menu.forEach(item=>{
+    item.addEventListener("click", ()=>{
+        linhas3.classList.toggle("active");
+        tlm_menu.classList.toggle("active");
+    })
 })
