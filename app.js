06a24bce680601d7fad8b9a31315cc795671bd89
Maicle72 <67045023+Maicle72@users.noreply.github.com>
2021-08-03 18:23:17 +0100

    Fix some things
    Trabalho com o Manel.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const linhas3 = document.querySelector(".header .nav-bar .nav-list .linhas3");
 const tlm_menu = document.querySelector(".header .nav-bar .nav-list ul");
 const menu = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
 const header = document.querySelector(".header.container-fluid");
+const nomeBrand = document.getElementById("Nome_Brand");
+const logoBrand = document.getElementById("logoImageHeader");
 
 linhas3.addEventListener("click",()=>{
     linhas3.classList.toggle("active");
@@ -12,7 +14,11 @@ document.addEventListener("scroll", ()=>{
     var scroll_position = window.scrollY;
     if(scroll_position > 500) {
         header.style.backgroundColor = "#1f1e1e";
+        nomeBrand.style.visibility = "visible";
+        logoBrand.style.visibility = "visible";
     }else{
+        logoBrand.style.visibility = "hidden";  
+        nomeBrand.style.visibility = "hidden";
         header.style.backgroundColor = "transparent";
     }
 });
